708b5870c70e3f8c0ebd61fc68dcdef0c0014419
Ruben de Villiers <ruben@notsotraditional.com>
2024-08-08 16:01:07 +0200

    Added a InfoCard component for dashboards

---
+++ app/components/InfoCard.tsx
@@ -0,0 +1,15 @@
+import { Card } from "flowbite-react";
+
+type InfoCardProps = {
+  title: string
+  value: string
+}
+
+const InfoCard: React.FC<InfoCardProps> = ({ title, value }) : React.ReactElement => {
+  return <Card>
+        <h5 className="font-bold text-primary">{title}</h5>
+        <p className="text-2xl">{value}</p>
+    </Card>
+}
+
+export default InfoCard
